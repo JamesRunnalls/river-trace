39ff5d803186638b0aa9c28d619477164ff55b27
JamesRunnalls <james.runnalls@eawag.ch>
2022-01-02 12:21:20 +0100

    Process turbidity

--- tests/acolite.py
+++ tests/acolite.py
@@ -7,20 +7,26 @@ water_parameter = "TUR_Dogliotti2015"
 river = "data/river.geojson"
 direction = "N"
 
-runs = [{"d": 20, "s": 1, "i": "ldq"},
-        {"d": 25, "s": 1, "i": "ldq"},
-        {"d": 30, "s": 1, "i": "ldq"},
-        {"d": 20, "s": 2, "i": "ldq"},
-        {"d": 25, "s": 2, "i": "ldq"},
-        {"d": 30, "s": 2, "i": "ldq"}]
+inputs = [["ldq", 2], ["ldr", 4], ["mds", 3], ["mdt", 4], ["mdu", 3]]
+runs = []
+
+for i in inputs:
+    for s in range(1, i[1] + 1):
+        for d in [20, 25, 30]:
+            runs.append({"d": d, "s": s, "i": i[0]})
 
 for run in runs:
-    folder = folder_t.format(run["i"], run["s"], run["d"], run["d"])
-    files = list(filter(lambda file: "L2ACOLITE" in file, os.listdir(folder)))
-    out_file_name = "path_" + "_".join([str(run["i"]), str(run["s"]), str(run["d"])])
-    for file in files:
-        trace(os.path.join(folder, file), water_parameter, river, direction, threshold="None", start_jump=0, plots=False,
-              out_folder=out_folder, out_file_name=out_file_name)
+    try:
+        folder = folder_t.format(run["i"], run["s"], run["d"], run["d"])
+        files = list(filter(lambda file: "L2ACOLITE" in file, os.listdir(folder)))
+        out_file_name = "path_" + "_".join([str(run["i"]), str(run["s"]), str(run["d"])])
+        for file in files:
+            trace(os.path.join(folder, file), water_parameter, river, direction, threshold="None", start_jump=0, plots=True,
+                  out_folder=out_folder, out_file_name=out_file_name)
+    except Exception as e:
+        print("Failed on run: ".format(run))
+        print(e)
+
 
 
 
---
+++ tests/extract.py
@@ -0,0 +1,42 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from rivertrace.functions import get_pixel_values, parse_netcdf, smooth, log
+
+folder_t = "/media/jamesrunnalls/JamesSSD/Eawag/EawagRS/Sencast/build/DIAS/output_data/Tshikapa_L1C_S2_tshikapa_{}_{}_2021-07-{}_2021-07-{}/L2ACOLITE"
+path_folder = "data/paths"
+out_folder = "data/csv"
+variables = [{"key": "TUR_Dogliotti2015", "label": "Turbidity", "title": "Turbidity along the Chicapa River as it passes the Catoca Mine."},
+             {"key": "hue_angle", "label": "Hue Angle", "title": "Hue Angle along the Chicapa River as it passes the Catoca Mine."}]
+
+paths = os.listdir(path_folder)
+paths.sort()
+
+for date in [20, 25, 30]:
+    dp = [k for k in paths if str(date) in k]
+    lat_arr, lon_arr, tur_arr, hue_arr, box = [], [], [], [], []
+    for path in dp:
+        with open(os.path.join(path_folder, path)) as json_file:
+            p = json.load(json_file)
+        ps = path.split(".")[0].split("_")
+        folder = folder_t.format(ps[1], ps[2], ps[3], ps[3])
+        file = list(filter(lambda f: "L2ACOLITE" in f, os.listdir(folder)))[0]
+        tur, lat, lon, mask = parse_netcdf(os.path.join(folder, file), "TUR_Dogliotti2015")
+        hue, lat, lon, mask = parse_netcdf(os.path.join(folder, file), "hue_angle")
+        lat_arr = lat_arr + list(get_pixel_values(p, lat))
+        lon_arr = lon_arr + list(get_pixel_values(p, lon))
+        tur_arr = tur_arr + list(get_pixel_values(p, tur, min=0, max=10000, group=1))
+        hue_arr = hue_arr + list(get_pixel_values(p, hue, group=1))
+        box = box + ["{}_{}".format(ps[1], ps[2])] * len(lat_arr)
+    df = pd.DataFrame(list(zip(box, lat_arr, lon_arr, tur_arr, hue_arr)),
+                      columns=['Bounding Box', 'Latitude', 'Longitude', 'Turbidity', 'Hue Angle'])
+
+    df = df.drop_duplicates(subset=['Latitude', 'Longitude'], keep='first').reset_index(drop=True)
+    df.to_csv(os.path.join(out_folder, "data_{}-07-2021.csv".format(date)))
+
+
+
+
+
